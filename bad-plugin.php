7c4c79c03f1a3ef872dec9e056901427fb94815c
Rahul Bansal <rahul.bansal@rtcamp.com>
2015-03-01 23:52:01 +0530

    first

---
+++ bad-plugin.php
@@ -0,0 +1,52 @@
+<?php
+/*
+	Plugin Name: Bad Plugin
+	Plugin URI: https://github.com/rahul286/bad-plugin
+  Description: Goal of this plugin to show how a small but bad plugin affect your site's performance
+	Author: rtCamp
+	Version: 0.1
+	Author URI: http://rtcamp.com/
+	Text Domain: bad-plugin
+	Domain Path: /languages
+ */
+
+ /**
+  * Adds Bad_Widget widget.
+  */
+ class Bad_Widget extends WP_Widget {
+
+ 	/**
+ 	 * Register widget with WordPress.
+ 	 */
+ 	function __construct() {
+ 		parent::__construct(
+ 			'Bad_widget', // Base ID
+ 			__( 'Bad Widget', 'bad-plugin' ), // Name
+ 			array( 'description' => __( 'A Bad widget which slows down your site', 'bad-plugin' ), ) // Args
+ 		);
+ 	}
+
+ 	/**
+ 	 * Front-end display of widget.
+ 	 *
+ 	 * @see WP_Widget::widget()
+ 	 *
+ 	 * @param array $args     Widget arguments.
+ 	 * @param array $instance Saved values from database.
+ 	 */
+ 	public function widget( $args, $instance ) {
+ 		echo $args['before_widget'];
+ 		if ( ! empty( $instance['title'] ) ) {
+ 			echo $args['before_title'] . apply_filters( 'widget_title', $instance['title'] ). $args['after_title'];
+ 		}
+ 		echo __( 'This is bad widegt! Ok, not at the monet but this is supposed to slow down yours ite!', 'bad_plugin' );
+ 		echo $args['after_widget'];
+ 	}
+
+ } // class Bad_Widget
+
+ // register Bad_Widget widget
+ function register_bad_widget() {
+     register_widget( 'Bad_Widget' );
+ }
+ add_action( 'widgets_init', 'register_bad_widget' );
---
+++ readme.txt
@@ -0,0 +1,13 @@
+=== Plugin Name ===
+Contributors: rahul286
+Requires at least: 3.0.1
+Tested up to: 4.1
+Stable tag: 0.1
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Goal of this plugin to show how a small but bad plugin affect your site's performance
+
+== Description ==
+
+Goal of this plugin to show how a small but bad plugin affect your site's performance
