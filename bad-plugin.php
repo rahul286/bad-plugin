687e3df1616624993fbef114ea58b2c0f069d7ae
Rahul Bansal <rahul.bansal@rtcamp.com>
2015-03-03 01:02:57 +0530

    cleaup

--- bad-plugin.php
+++ bad-plugin.php
@@ -8,61 +8,49 @@
 	Author URI: http://rtcamp.com/
 	Text Domain: bad-plugin
 	Domain Path: /languages
+*/
+/**
+ * Adds Bad_Widget widget.
  */
-
- /**
-  * Adds Bad_Widget widget.
-  */
- class Bad_Widget extends WP_Widget {
-
- 	/**
- 	 * Register widget with WordPress.
- 	 */
- 	function __construct() {
- 		parent::__construct(
- 			'Bad_widget', // Base ID
- 			__( 'Bad Widget', 'bad-plugin' ), // Name
- 			array( 'description' => __( 'A Bad widget which slows down your site', 'bad-plugin' ), ) // Args
- 		);
- 	}
-
- 	/**
- 	 * Front-end display of widget.
- 	 *
- 	 * @see WP_Widget::widget()
- 	 *
- 	 * @param array $args     Widget arguments.
- 	 * @param array $instance Saved values from database.
- 	 */
- 	public function widget( $args, $instance ) {
-    global $wpdb;
- 		echo $args['before_widget'];
-
- 		if ( ! empty( $instance['title'] ) ) {
- 			echo $args['before_title'] . apply_filters( 'widget_title', $instance['title'] ). $args['after_title'];
- 		}
- 		echo __( 'This is bad widegt! Ok, not at the monet but this is supposed to slow down yours ite!', 'bad_plugin' );
-
-     /*
-        bad-query
-      */
-    // $res = $wpdb->get_results("select distinct post_title, display_name from wp_posts p, wp_users u WHERE p.post_author = u.ID AND p.post_status = 'publish' AND p.post_type = 'post' AND p.post_content LIKE '%microsoft%' OR  p.post_content LIKE '%apple%'",ARRAY_A);
-
-
-    /*
-       good-query
+class Bad_Widget extends WP_Widget {
+    /**
+     * Register widget with WordPress.
      */
-   $res = $wpdb->get_results("select post_title, display_name from wp_posts p, wp_users u WHERE p.post_author = u.ID AND p.post_status = 'publish' AND p.post_type = 'post' AND (p.post_content LIKE '%microsoft%' OR  p.post_content LIKE '%apple%')",ARRAY_A);
-
-    var_dump ($res);
-
- 		echo $args['after_widget'];
- 	}
-
- } // class Bad_Widget
-
- // register Bad_Widget widget
- function register_bad_widget() {
-     register_widget( 'Bad_Widget' );
- }
- add_action( 'widgets_init', 'register_bad_widget' );
+    function __construct() {
+        parent::__construct('Bad_widget', // Base ID
+        __('Bad Widget', 'bad-plugin'), // Name
+        array('description' => __('A Bad widget which slows down your site', 'bad-plugin'),) // Args
+        );
+    }
+    /**
+     * Front-end display of widget.
+     *
+     * @see WP_Widget::widget()
+     *
+     * @param array $args     Widget arguments.
+     * @param array $instance Saved values from database.
+     */
+    public function widget($args, $instance) {
+        global $wpdb;
+        echo $args['before_widget'];
+        if (!empty($instance['title'])) {
+            echo $args['before_title'] . apply_filters('widget_title', $instance['title']) . $args['after_title'];
+        }
+        echo __('This is bad widegt! Ok, not at the monet but this is supposed to slow down yours ite!', 'bad_plugin');
+        /*
+        bad-query
+        */
+        // $res = $wpdb->get_results("select distinct post_title, display_name from wp_posts p, wp_users u WHERE p.post_author = u.ID AND p.post_status = 'publish' AND p.post_type = 'post' AND p.post_content LIKE '%microsoft%' OR  p.post_content LIKE '%apple%'",ARRAY_A);
+        /*
+        good-query
+        */
+        $res = $wpdb->get_results("select post_title, display_name from wp_posts p, wp_users u WHERE p.post_author = u.ID AND p.post_status = 'publish' AND p.post_type = 'post' AND (p.post_content LIKE '%microsoft%' OR  p.post_content LIKE '%apple%')", ARRAY_A);
+        var_dump($res);
+        echo $args['after_widget'];
+    }
+} // class Bad_Widget
+// register Bad_Widget widget
+function register_bad_widget() {
+    register_widget('Bad_Widget');
+}
+add_action('widgets_init', 'register_bad_widget');
