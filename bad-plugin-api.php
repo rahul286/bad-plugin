2e5d44b5d317a42e44b43863bed2371801e64e9a
Rahul Bansal <rahul.bansal@rtcamp.com>
2015-03-03 17:41:08 +0530

    update api example

--- bad-plugin-api.php
+++ bad-plugin-api.php
@@ -26,8 +26,8 @@ function bad_api_call( $post_id ) {
   add_action( 'admin_notices', 'bad_api_notice' );
 }
 
-add_action('publish_post', 'register_bad_api_call');
-add_action( 'save_post', 'register_bad_api_call' );
+add_action('publish_post', 'bad_api_call');
+add_action( 'save_post', 'bad_api_call' );
 
 
 function rt_hello() {
